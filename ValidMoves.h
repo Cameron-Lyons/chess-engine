6709ac42c3b41e6d7e41c9f5e73d29a07e170573
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-08 22:59:01 -0500

    add validmoves

---
+++ ValidMoves.h
@@ -0,0 +1,12 @@
+#include "PieceMoves.h"
+#include "ChessBoard.h"
+
+static bool BlackAttackBoard[64];
+static bool WhiteAttackBoard[64];
+
+static int BlackKingPosition;
+static int WhiteKingPosition;
+
+static bool AnalyzeMove(Board board, int dest, Piece piece){
+    
+}
