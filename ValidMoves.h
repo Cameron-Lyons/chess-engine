58269004c5e1ecba02033c1f97dcfe0ed9492c54
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-09 17:12:46 -0500

    ckecked if sides can castle

--- ValidMoves.h
+++ ValidMoves.h
@@ -101,3 +101,68 @@ void AnalyzeMovePawn(Board board, int dest, Piece piece){
     return;
 }
 
+
+void GenValidMovesKingCastle(Board board, Piece king){
+    if (king.PieceType == NONE){
+        return;
+    }
+    if (king.moved){
+        return;
+    }
+    if (king.PieceColor == WHITE && ~board.whiteCanCastle){
+        return;
+    }
+    if (king.PieceColor == BLACK && ~board.blackCanCastle){
+        return;
+    }
+    if (king.PieceColor == WHITE && board.whiteChecked){
+        return;
+    }
+    if (king.PieceColor == BLACK && board.blackChecked){
+        return;
+    }
+    if (king.PieceColor == BLACK){
+        if (board.squares[63].Piece.PieceType == ROOK){
+            if (board.squares[62].Piece.PieceType == NONE
+                && board.squares[61].Piece.PieceType == NONE){
+                if (!BlackAttackBoard[61] && !BlackAttackBoard[62] && !BlackAttackBoard[63]){
+                    king.ValidMoves.push_back(62);
+                    WhiteAttackBoard[62] = true;
+                }
+            }
+        }
+        if (board.squares[56].Piece.PieceType == ROOK){
+            if (board.squares[57].Piece.PieceType == NONE
+                && board.squares[58].Piece.PieceType == NONE
+                && board.squares[59].Piece.PieceType == NONE){
+                if (!BlackAttackBoard[59] && !BlackAttackBoard[58] && !BlackAttackBoard[57]){
+                    king.ValidMoves.push_back(58);
+                    WhiteAttackBoard[58] = true;
+                }
+            }
+        }
+    if (king.PieceColor == WHITE){
+        if (board.squares[7].Piece.PieceType == ROOK){
+            if (board.squares[6].Piece.PieceType == NONE
+                && board.squares[5].Piece.PieceType == NONE){
+                if (!WhiteAttackBoard[5] && !WhiteAttackBoard[6] && !WhiteAttackBoard[7]){
+                    king.ValidMoves.push_back(6);
+                    BlackAttackBoard[6] = true;
+                }
+            }
+        }
+        if (board.squares[0].Piece.PieceType == ROOK){
+            if (board.squares[1].Piece.PieceType == NONE
+                && board.squares[2].Piece.PieceType == NONE
+                && board.squares[3].Piece.PieceType == NONE){
+                if (!WhiteAttackBoard[3] && !WhiteAttackBoard[2] && !WhiteAttackBoard[1]){
+                    king.ValidMoves.push_back(2);
+                    BlackAttackBoard[2] = true;
+                }
+            }
+        }
+    }
+    }
+}
+
+
