4a1e9aade61f395796ae2782b7b87466e1e65926
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-10 17:40:47 -0500

    take pieces

--- MoveContent.h
+++ MoveContent.h
@@ -7,4 +7,42 @@ struct PieceMoving{
     bool moved;
     ChessPieceType pieceType;
     ChessPieceColor pieceColor;
-}
+
+    PieceMoving(ChessPieceColor pieceColor, ChessPieceType pieceType, bool moved){
+        this->dest = dest;
+        this->src = src;
+        this->moved = moved;
+        this->pieceType = pieceType;
+        this->pieceColor = pieceColor;
+    }
+    PieceMoving(ChessPieceType pieceType){
+        pieceType = pieceType;
+        pieceColor = WHITE;
+        src = 0;
+        dest = 0;
+        moved = false;
+    }
+};
+
+
+struct PieceTaken{
+    bool moved;
+    ChessPieceType pieceType;
+    ChessPieceColor pieceColor;
+    int position;
+
+    PieceTaken(ChessPieceColor pieceColor, ChessPieceType pieceType, bool moved){
+        this->moved = moved;
+        this->pieceType = pieceType;
+        this->pieceColor = pieceColor;
+        this->position = position;
+    }
+
+    PieceTaken(ChessPieceType pieceType){
+        pieceType = pieceType;
+        pieceColor = WHITE;
+        position = 0;
+        moved = false;
+    }
+};
+
