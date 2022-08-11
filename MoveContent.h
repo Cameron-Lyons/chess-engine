d4eae8e14d1601ad31a79f4892b638b46b04c511
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-10 20:28:22 -0500

    get pgn

--- MoveContent.h
+++ MoveContent.h
@@ -1,4 +1,5 @@
 #include "ChessPiece.h"
+#include <string>
 
 
 struct PieceMoving{
@@ -45,4 +46,40 @@ struct PieceTaken{
         moved = false;
     }
 };
+
+
+std::string PortableGameNotation(){
+    std::string pgn;
+    int srcCol = MovingPiece.src%8;
+    int srcRow = MovingPiece.src/8;
+    int destCol = MovingPiece.dest%8;
+    int destRow = MovingPiece.dest/8;
+
+    if (MovingPieceSecondary.PieceColor == WHITE){
+        if (MovingPieceSecondary.src == 7){
+            pgn += "O-O";
+        }
+        else if (MovingPieceSecondary.src == 0){
+            pgn += "O-O-O";
+        }
+    }
+    else{
+        if (MovingPieceSecondary.src == 56){
+            pgn += "O-O";
+        }
+        else if (MovingPieceSecondary.src == 63){
+            pgn += "O-O-O";
+        }
+    }
+    if (TakenPiece.PieceType != NONE){
+        pgn += "x";
+    }
+    pgn += GetColumnFromInt(destCol + 1);
+    pgn += destRow;
+    if (PawnPromoted){
+        pgn += "Q";
+    }
+    return pgn;
+}
+
 
